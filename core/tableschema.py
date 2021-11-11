f5a9e000d8ef6b4234a46324e7cde6f3cba4f365
ibmzhangjun <ibmzhangjun@139.com>
2021-11-11 18:17:32 +0800

    add full-stack-example

--- core/tableschema.py
+++ core/tableschema.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from sqlalchemy import Table
+#  #
+#  Copyright (C) 2021 ZinoHome, Inc. All Rights Reserved
+#  #
+#  @Time    : 2021
+#  @Author  : Zhang Jun
+#  @Email   : ibmzhangjun@139.com
+#  @Software: Neptune
+
 
 class TableSchema(object):
-    def __init__(self, name, type):
-        self._name = name
-        self._type = type
+    def __init__(self, table_name, table_type):
+        self._name = table_name
+        self._type = table_type
         self._primarykeys = 'N/A'
         self._indexes = 'N/A'
         self._columns = 'N/A'
+        self._dict = 'N/A'
 
     @property
     def name(self):
@@ -19,33 +27,68 @@ class TableSchema(object):
     def name(self, value):
         self._name = value
 
-    def gettype(self):
-        return self.__type
+    @property
+    def type(self):
+        return self._type
 
-    def getprimarykeys(self):
-        return self.__primarykeys
+    @type.setter
+    def type(self, value):
+        self._type = value
 
-    def getindexes(self):
-        return self.__indexes
+    @property
+    def primarykeys(self):
+        return self._primarykeys
 
-    def getcolumns(self):
-        return self.__columns
+    @primarykeys.setter
+    def primarykeys(self, value):
+        self._primarykeys = value
 
-    def setprimarykeys(self, primarykeys):
-        self.__primarykeys = primarykeys
+    @primarykeys.deleter
+    def primarykeys(self):
+        self._primarykeys = 'N/A'
 
-    def setindexes(self, indexes):
-        self.__indexes = indexes
+    @property
+    def indexes(self):
+        return self._indexes
 
-    def setcolumns(self, columns):
-        self.__columns = columns
+    @indexes.setter
+    def indexes(self, value):
+        self._indexes = value
 
+    @indexes.deleter
+    def indexes(self):
+        self._indexes = 'N/A'
+
+    @property
+    def columns(self):
+        return self._columns
+
+    @columns.setter
+    def columns(self, value):
+        self._columns = value
+
+    @columns.deleter
+    def columns(self):
+        self._columns = 'N/A'
+
+    @property
+    def dict(self):
+        return self._dict
+
+    @dict.setter
+    def dict(self, value):
+        self._dict = value
+
+    @dict.deleter
+    def dict(self):
+        self._dict = 'N/A'
 
     def table2json(self):
         return {
-            'name': self.__name,
-            'type': self.__type,
-            'primarykeys': self.__primarykeys,
-            'indexes': self.__indexes,
-            'columns': self.__columns
+            'name': self._name,
+            'type': self._type,
+            'primarykeys': self._primarykeys,
+            'indexes': self._indexes,
+            'columns': self._columns,
+            'dict': self._dict
         }
---
+++ tests/test_tableschema.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class TestTableSchema(TestCase):
+    def test_table_schema(self):
+        self.fail()
