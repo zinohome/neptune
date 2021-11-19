39138a972f44de5f621e0a3bfb84c3dac5b92763
zhangjun <ibmzhangjun@139.com>
2021-11-19 11:41:56 +0800

    delete draft

--- tests/test_dbmeta.py
+++ tests/test_dbmeta.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+#  #
+#  Copyright (C) 2021 ZinoHome, Inc. All Rights Reserved
+#  #
+#  @Time    : 2021
+#  @Author  : Zhang Jun
+#  @Email   : ibmzhangjun@139.com
+#  @Software: Neptune
+
 from core import dbmeta
 from config import config
 from util import log
