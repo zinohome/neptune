f51b59426818271d0da7c42657c9329930286834
zhangjun <ibmzhangjun@139.com>
2021-11-19 14:35:27 +0800

    update tests

--- core/userfunc.py
+++ core/userfunc.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+#  #
+#  Copyright (C) 2021 ZinoHome, Inc. All Rights Reserved
+#  #
+#  @Time    : 2021
+#  @Author  : Zhang Jun
+#  @Email   : ibmzhangjun@139.com
+#  @Software: Neptune
+
 import sys
 import traceback
 from fastapi.encoders import jsonable_encoder
---
+++ tests/test_userfunc.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#  #
+#  Copyright (C) 2021 ZinoHome, Inc. All Rights Reserved
+#  #
+#  @Time    : 2021
+#  @Author  : Zhang Jun
+#  @Email   : ibmzhangjun@139.com
+#  @Software: Neptune
+from config import config
+from util import log
+from core import userfunc
+
+'''config'''
+cfg = config.Config()
+
+'''logging'''
+log = log.Logger(level=cfg.application['app_log_level'])
+
+class TestUserFunc:
+    def test_user_func(self):
+        uf = userfunc.UserFunc('orders')
+        json = uf.query('select * from orders where orderNumber > :id limit 10', {'id': 10000})
+        log.logger.debug(json)
+        assert True
