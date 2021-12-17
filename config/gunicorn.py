e507a80cb90c6cfc40a86c20ccfc350df1924abe
zhangjun <ibmzhangjun@139.com>
2021-12-17 11:26:31 +0800

    update db diagram

--- config/gunicorn.py
+++ config/gunicorn.py
@@ -3,7 +3,7 @@
 
 bind = '0.0.0.0:8890'      # 绑定ip和端口号
 backlog = 512                # 监听队列
-chdir = '/opt/neptun'  # gunicorn要切换到的目的工作目录
+chdir = '/opt/neptune'  # gunicorn要切换到的目的工作目录
 timeout = 300      # 超时
 worker_class = 'uvicorn.workers.UvicornWorker'  # 使用gevent模式，还可以使用sync 模式，默认的是sync模式
 # workers = multiprocessing.cpu_count() * 2 + 1    # 进程数
